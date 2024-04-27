7dfd7031f9fa4efbcd51aac322d5dc51cdaf307c
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2024-04-27 21:29:08 +0600

    Migration done

--- database/migrations/2024_04_27_123313_create_policies.php
+++ database/migrations/2024_04_27_123313_create_policies.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('policies', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['about', 'refund', 'terms', 'how to buy', 'contract', 'complain']);
+            $table->longText('des');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('policies');
     }
 };
